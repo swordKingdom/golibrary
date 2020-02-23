5a52ed0be83d58136228256b933ba1a1d7d4b913
kajun <2587332053@qq.com>
2020-02-23 22:26:20 +0800

    fix bug

--- config/init.go
+++ config/init.go
@@ -1,53 +1,50 @@
-package configloader
+package config
 
 import (
-	"errors"
 	"fmt"
 	"strings"
 
-	"this_is_a_explame/lib/configloader/loader"
+	"golibrary/config/loader"
 )
 
-type generatorFunc func() loader.ConfLoader
+type GeneratorFunc func() loader.ConfLoader
 
-var confLoaderMap = make(map[string]generatorFunc)
+var confLoaderMap = make(map[string]GeneratorFunc)
 
 const (
 	//YamlConfType yaml文件类型
 	YamlConfType = "yml"
+	//todo:未支持
 	//XMLConfType xml文件类型
 	XMLConfType = "xml"
 	//TomlConfType toml文件类型
 	TomlConfType = "toml"
-
 	//FileNameDefultStepSep 文件后缀分割符号
 	FileNameDefultStepSep = "."
 )
 
 //LoadConfig 加载配置文件
 func LoadConfig(file string) (loader.ConfLoader, error) {
-	tmpStr := strings.Split(file, FileNameDefultStepSep)
-	if len(tmpStr) < 2 {
+	tmpStrArr := strings.Split(file, FileNameDefultStepSep)
+	if len(tmpStrArr) < 2 {
 		return nil, fmt.Errorf("conf file name:%v  error", file)
 	}
-	confType := tmpStr[len(tmpStr)-1]
+	confType := tmpStrArr[len(tmpStrArr)-1]
 	if generator, ok := confLoaderMap[confType]; ok {
 		l := generator()
 		err := l.LoadConfigFromFile(file)
 		return l, err
 	} else {
-		return nil, errors.New("unsupport type conf file")
+		return nil, fmt.Errorf("unsupport %v type conf file",confType)
 	}
 }
 
-func RegisteConfLoaderGenerator(confType string, f generatorFunc) {
+func RegisteConfLoaderGenerator(confType string, generator GeneratorFunc) {
 	if _, ok := confLoaderMap[confType]; !ok {
-		confLoaderMap[confType] = f
+		confLoaderMap[confType] = generator
 	}
 }
 
 func init() {
 	confLoaderMap[YamlConfType] = loader.NewYamlLoader
-	confLoaderMap[XMLConfType] = loader.NewXMLLoader
-	confLoaderMap[TomlConfType] = loader.NewTomlLoader
 }
